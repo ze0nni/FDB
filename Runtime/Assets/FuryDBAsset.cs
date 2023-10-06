23949c0934bd2281730b1066fdc5e3f1969093c8
ze0nni <ze0nni@gmail.com>
2023-10-06 22:44:49 +0300

    same Unity guis for different types

--- Runtime/Assets/FuryDBAsset.cs
+++ Runtime/Assets/FuryDBAsset.cs
@@ -14,21 +14,21 @@ namespace FDB
         public List<string> Errors;
         public List<FuryDBEntryAsset> Entries;
 
-        private Dictionary<string, UnityEngine.Object> _dependencyCahce;
+        private Dictionary<(string, Type), UnityEngine.Object> _dependencyCahce;
         public UnityEngine.Object ResolveDependency(string guid, Type type)
         {
             if (_dependencyCahce == null)
             {
-                _dependencyCahce = new Dictionary<string, UnityEngine.Object>();
+                _dependencyCahce = new Dictionary<(string, Type), UnityEngine.Object>();
                 foreach (var e in Entries)
                 {
                     foreach (var r in e.Dependency)
                     {
-                        _dependencyCahce[r.GUID] = r.Object;
+                        _dependencyCahce[(r.GUID, r.Object.GetType())] = r.Object;
                     }
                 }
             }
-            _dependencyCahce.TryGetValue(guid, out var result);
+            _dependencyCahce.TryGetValue((guid, type), out var result);
             return result;
         }
     }

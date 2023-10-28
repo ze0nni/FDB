db830af647e12724915599599310c533354ebcb2
ze0nni <ze0nni@gmail.com>
2023-10-28 16:57:51 +0300

    Simplify generators. Add readable errors

---
+++ Editor/EditorDB.Generator.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace FDB.Editor
+{
+    public partial class EditorDB<T>
+    {
+        public  static IEnumerable<(string, IFuryGenerator<T>)> ResolveGenerators()
+        {
+            yield return (MetaData.CsGenPath, new DefaultGenerator());
+
+            foreach (var ga in typeof(T)
+                .GetCustomAttributes(typeof(FuryGeneratorAttribute), false)
+                .Cast<FuryGeneratorAttribute>())
+            {
+                var generator = default(IFuryGenerator<T>);
+                try
+                {
+                    if (!typeof(IFuryGenerator<T>).IsAssignableFrom(ga.GeneratorType))
+                    {
+                        throw new ArgumentException($"Generator {ga.GeneratorType} must implements IFuryGenerator<{typeof(T)}>");
+                    }
+                    generator = (IFuryGenerator<T>)Activator.CreateInstance(ga.GeneratorType);
+                } catch
+                {
+                    Debug.LogError($"Error when create generator {ga.GeneratorType} => {ga.CsPath}");
+                    throw;
+                }
+                yield return (ga.CsPath, generator);
+            }
+        }
+
+        private class DefaultGenerator : IFuryGenerator<T>
+        {
+            public void Execute(IndentStringBuilder sb, T db)
+            {
+                var dbType = typeof(T);
+
+                sb.AppendLine("using FDB;");
+                if (!string.IsNullOrEmpty(dbType.Namespace))
+                {
+                    sb.AppendLine($"namespace {dbType.Namespace}");
+                    sb.AppendLine("{");
+                    sb.BeginIndent();
+                }
+
+                {
+                    sb.AppendLine($"public partial class {dbType.Name}");
+                    sb.AppendLine("{");
+                    sb.BeginIndent();
+                    {
+                        sb.AppendLine($"public static partial class Kinds ");
+                        sb.AppendLine("{");
+                        sb.BeginIndent();
+                        foreach (var field in dbType.GetFields())
+                        {
+                            var index = field.GetValue(db) as Index;
+                            if (index == null)
+                            {
+                                continue;
+                            }
+                            sb.AppendLine($"public static partial class {field.Name}");
+                            sb.AppendLine("{");
+                            sb.BeginIndent();
+                            var modelType = index.GetType().GetGenericArguments()[0];
+                            var kindField = modelType.GetField("Kind");
+                            foreach (var config in index.All())
+                            {
+                                var kind = (Kind)kindField.GetValue(config);
+                                if (!kind.CanExport)
+                                {
+                                    sb.AppendLine($"// Skip kind '{kind.Value}'");
+                                }
+                                else if (index.IsDuplicateKind(kind.Value))
+                                {
+                                    sb.AppendLine($"// Skip duplicate '{kind.Value}'");
+                                    Debug.LogWarning($"Skip duplicate kind='{kind.Value}' of {modelType.Name}");
+                                }
+                                else
+                                {
+                                    sb.AppendLine($"public static Kind<{modelType.Name}> {kind.Value} = new Kind<{modelType.Name}>(\"{kind.Value}\");");
+                                }
+                            }
+                            sb.EndIndent();
+                            sb.AppendLine("}");
+
+                        }
+                        sb.EndIndent();
+                        sb.AppendLine("}");
+                    }
+                    sb.EndIndent();
+                    sb.AppendLine("}");
+                }
+
+                if (!string.IsNullOrEmpty(dbType.Namespace))
+                {
+                    sb.EndIndent();
+                    sb.AppendLine("}");
+                }
+            }
+        }
+    }
+}
--- Editor/EditorDB.cs
+++ Editor/EditorDB.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using UnityEditor;
 using UnityEngine;
 
@@ -18,13 +17,14 @@ namespace FDB.Editor
         }
     }
 
-    public class EditorDB<T>
+    public partial class EditorDB<T>
     {
         public static long Version { get; private set; }
         public static bool IsDirty { get; private set; }
 
         static T _db;
         static DBResolver _resolver;
+        static (string Path, IFuryGenerator<T> Generator)[] _generators;
 
         static FuryDBAttribute MetaData
         {
@@ -45,11 +45,24 @@ namespace FDB.Editor
             get {
                 if (_db == null)
                 {
-                    if (File.Exists(MetaData.SourcePath)) {
-                        _db = Load(out _resolver);
-                    } else
+                    try
                     {
-                        _db = DBResolver.New<T>(out _resolver);
+                        if (File.Exists(MetaData.SourcePath))
+                        {
+                            _db = Load(out _resolver);
+                            _generators = ResolveGenerators().ToArray();
+                        }
+                        else
+                        {
+                            _db = DBResolver.New<T>(out _resolver);
+                            _generators = ResolveGenerators().ToArray();
+                        }
+                    } catch
+                    {
+                        _db = default;
+                        _resolver = default;
+                        _generators = default;
+                        throw;
                     }
                 }
                 return _db;
@@ -116,18 +129,21 @@ namespace FDB.Editor
             }
 
             var generatesFiles = new List<string>();
-            GenerateCs(MetaData.CsGenPath, _db);
             generatesFiles.Add(MetaData.CsGenPath);
 
-            foreach (var ga in typeof(T)
-                .GetCustomAttributes(typeof(FuryGeneratorAttribute), false)
-                .Cast<FuryGeneratorAttribute>())
+            foreach (var ga in _generators)
             {
-                var sb = new IndentStringBuilder();
-                var generator = (IFuryGenerator<T>)Activator.CreateInstance(ga.GeneratorType);
-                generator.Execute(sb, _db);
-                File.WriteAllText(ga.CsPath, sb.ToString());
-                generatesFiles.Add(ga.CsPath);
+                try
+                {
+                    var sb = new IndentStringBuilder();
+                    ga.Generator.Execute(sb, _db);
+                    File.WriteAllText(ga.Path, sb.ToString());
+                    generatesFiles.Add(ga.Path);
+                } catch (Exception exc)
+                {
+                    Debug.LogError($"Error when execute generator {ga.Generator} => {ga.Path}");
+                    Debug.LogException(exc);
+                }
             }
 
             foreach (var f in generatesFiles)
@@ -142,77 +158,6 @@ namespace FDB.Editor
             {
                 Load();
             }
-        }
-
-        public static void GenerateCs(string path, T db)
-        {
-            var sb = new IndentStringBuilder();
-
-            var dbType = typeof(T);
-
-            sb.AppendLine("using FDB;");
-            if (!string.IsNullOrEmpty(dbType.Namespace))
-            {
-                sb.AppendLine($"namespace {dbType.Namespace}");
-                sb.AppendLine("{");
-                sb.BeginIndent();
-            }
-
-            {
-                sb.AppendLine($"public partial class {dbType.Name}");
-                sb.AppendLine("{");
-                sb.BeginIndent();
-                {
-                    sb.AppendLine($"public static partial class Kinds ");
-                    sb.AppendLine("{");
-                    sb.BeginIndent();
-                    foreach (var field in dbType.GetFields())
-                    {
-                        var index = field.GetValue(db) as Index;
-                        if (index == null)
-                        {
-                            continue;
-                        }
-                        sb.AppendLine($"public static partial class {field.Name}");
-                        sb.AppendLine("{");
-                        sb.BeginIndent();
-                        var modelType = index.GetType().GetGenericArguments()[0];
-                        var kindField = modelType.GetField("Kind");
-                        foreach (var config in index.All())
-                        {
-                            var kind = (Kind)kindField.GetValue(config);
-                            if (!kind.CanExport)
-                            {
-                                sb.AppendLine($"// Skip kind '{kind.Value}'");
-                            }
-                            else if (index.IsDuplicateKind(kind.Value))
-                            {
-                                sb.AppendLine($"// Skip duplicate '{kind.Value}'");
-                                Debug.LogWarning($"Skip duplicate kind='{kind.Value}' of {modelType.Name}");
-                            }
-                            else
-                            {
-                                sb.AppendLine($"public static Kind<{modelType.Name}> {kind.Value} = new Kind<{modelType.Name}>(\"{kind.Value}\");");
-                            }
-                        }
-                        sb.EndIndent();
-                        sb.AppendLine("}");
-
-                    }
-                    sb.EndIndent();
-                    sb.AppendLine("}");
-                }
-                sb.EndIndent();
-                sb.AppendLine("}");
-            }
-
-            if (!string.IsNullOrEmpty(dbType.Namespace))
-            {
-                sb.EndIndent();
-                sb.AppendLine("}");
-            }
-
-            File.WriteAllText(path, sb.ToString());
         }
     }
 }
